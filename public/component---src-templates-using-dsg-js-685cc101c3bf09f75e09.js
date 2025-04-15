f63f834e63e1132276cc8000a23fb5114dbc5c9f
Abhay Singh <abhaysingh200009@gmail.com>
2025-04-15 18:15:45 -0500

    corrected UI

--- public/component---src-templates-using-dsg-js-685cc101c3bf09f75e09.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkgatsby_starter_default=self.webpackChunkgatsby_starter_default||[]).push([[759],{4808:function(e,t,a){a.r(t),a.d(t,{Head:function(){return s}});var n=a(7294),r=a(9505);const s=()=>n.createElement(r.Z,{title:"Using DSG"});t.default=()=>n.createElement("div",null,"Hi")}}]);
-//# sourceMappingURL=component---src-templates-using-dsg-js-685cc101c3bf09f75e09.js.map
