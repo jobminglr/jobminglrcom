47788437dcac697e75edc9de08f50b55f17fc943
Abhay Singh <abhaysingh200009@gmail.com>
2025-04-15 17:31:03 -0500

    qr-update

--- src/components/Layout1.js
+++ src/components/Layout1.js
@@ -4,6 +4,22 @@ import Footer from "./Footer";
 import { StaticImage } from 'gatsby-plugin-image'
 
 const Layout1 = ({ children }) => {
+  const getAppLink = () => {
+    const isAppleDevice = /iPhone|iPad|iPod/i.test(navigator.userAgent);
+    const isAndroidDevice = /Android/i.test(navigator.userAgent);
+
+    if (isAppleDevice) {
+      return "https://apps.apple.com/us/app/jobminglr/id6738838504";
+    } else if (isAndroidDevice) {
+      return "https://play.google.com/store/apps/details?id=com.jobminglr.in.android&utm_source=na_Med";
+    } else {
+      // Web browser, return the website link
+      return "https://www.jobminglr.app/";
+    }
+  };
+
+  const appLink = getAppLink();
+
   return (
     <>
       <Header />
@@ -12,7 +28,7 @@ const Layout1 = ({ children }) => {
       </main>
       <div className="fixed bottom-4 right-4 z-50">
         <a
-          href="https://apps.apple.com/us/app/jobminglr/id6738838504"
+          href={appLink}
           target="_blank"
           rel="noopener noreferrer"
         >
