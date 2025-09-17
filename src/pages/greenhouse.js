e0cfd2b3a7f22e9b33511d61cf40428b11f09968
Abhay Singh <abhaysingh200009@gmail.com>
2025-09-16 22:59:23 -0500

    build files

--- src/pages/greenhouse.js
+++ src/pages/greenhouse.js
@@ -22,7 +22,7 @@ const GreenhousePage = () => {
         title="Greenhouse Integration"
         description="JobMinglr + Greenhouse Integration"
       />
-      <section className="container mx-auto py-20 px-6 text-center">
+      <section className="container mx-auto py-20 px-6 text-center md:text-left">
         <h1 className="text-4xl font-extrabold mb-6 text-brandGreen" data-aos="fade-up">
           JobMinglr + Greenhouse Integration
         </h1>
