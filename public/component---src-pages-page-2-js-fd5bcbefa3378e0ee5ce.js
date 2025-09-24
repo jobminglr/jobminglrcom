e96b85db494294d63b7e718fe7b7e176f30fd62a
Abhay Singh <abhaysingh200009@gmail.com>
2025-09-24 11:11:01 -0500

    build files

---
+++ public/component---src-pages-page-2-js-fd5bcbefa3378e0ee5ce.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkgatsby_starter_default=self.webpackChunkgatsby_starter_default||[]).push([[771],{5283:function(e,t,a){a.r(t),a.d(t,{Head:function(){return u}});var l=a(6540),n=a(4194),r=a(3180),c=a(2484);const u=()=>l.createElement(c.A,{title:"Page two"});t.default=()=>l.createElement(r.A,null,l.createElement("h1",null,"Hi from the second page"),l.createElement("p",null,"Welcome to page 2"),l.createElement(n.Link,{to:"/"},"Go back to the homepage"))}}]);
+//# sourceMappingURL=component---src-pages-page-2-js-fd5bcbefa3378e0ee5ce.js.map
